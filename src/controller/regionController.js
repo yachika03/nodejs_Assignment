09ab2406d3d92837db64490eed28b0d5fad0e5f3
yachika03 <110588486+yachika03@users.noreply.github.com>
2022-12-20 23:35:30 +0530

    Update regionController.js

--- src/controller/regionController.js
+++ src/controller/regionController.js
@@ -12,26 +12,17 @@ const createRegion = async function (req, res) {
     
     let organizationId = req.params.organizationId;
     if (!organizationId)
-      return res.status(400).send({
-        status: false,
-        Message: "Please provide the organizationId. It's mandatory.",
-      });
+      return res.status(400).send({status: false,Message: "Please provide the organizationId. It's mandatory."});
     // if (!isValidObjectId(organizationId)) return res.status(400).send({ status: false, Message: `'${organizationId}' is not valid please check.` })
 
-    let findOrganization = await organizationModel.findById({
-      _id: organizationId,
-    });
+    let findOrganization = await organizationModel.findById({ _id: organizationId});
     if (!findOrganization)
-      return res
-        .status(404)
-        .send({ status: false, Message: "This organization is not present." });
+      return res.status(404).send({ status: false, Message: "This organization is not present." });
 
     if (Object.keys(data).length == 0)
-      return res.status(400).send({
-        status: false,
-        Message: "Please provide some data for creation.",
-      });
+      return res.status(400).send({status: false,Message: "Please provide some data for creation."});
 
+        
     let { property, cropId } = data;
 
     if (data.property) {
@@ -41,21 +32,12 @@ const createRegion = async function (req, res) {
           Message: "Region must be from {North, South, East, West}",
         });
       }
-      // if(fieldSize != Number)
-
-      // if(fieldSize){
-      //     if( isNaN(property.fieldSize)) return res.status(400).send({ status: false, Message: "please provide fieldSize in Number." })
-      //     property.fieldSize = property.fieldSize + "sqmt"
-      // }
+      
     }
 
-    let findCrop = await cropModel
-      .findOne({ _id: cropId })
-      .select({ updatedAt: 0, __v: 0, createdAt: 0 });
+    let findCrop = await cropModel.findOne({ _id: cropId }).select({ updatedAt: 0, __v: 0, createdAt: 0 });
     if (!findCrop)
-      return res
-        .status(404)
-        .send({ status: false, Message: "This crop is not present in DB." });
+      return res.status(404).send({ status: false, Message: "This crop is not present in DB." });
 
     let newRegion = {
       property: property,
@@ -63,78 +45,14 @@ const createRegion = async function (req, res) {
     };
 
     let saveDetails = await regionModel.create(newRegion);
-    console.log(saveDetails);
-    return res.status(201).send({
-      status: true,
-      Message: "Region is created successfully.",
-      data: saveDetails,
-    });
+    // console.log(saveDetails);
+    return res.status(201).send({status: true,Message: "Region is created successfully.",data: saveDetails});
   } catch (error) {
     // console.log("catch block")
     return res.status(500).send({ status: false, Message: error.Message });
   }
 };
 
-//=====================update-region======================================================================
-
-const updateRegion = async function (req, res) {
-  try {
-    let data = req.body;
-    let organizationId = req.params.organizationId;
-
-    let { regionId, property } = data;
-
-    if (!organizationId)
-      return res.status(400).send({
-        status: false,
-        Message: "Please provide the organizationId. It's mandatory.",
-      });
-    // if (!isValidObjectId(organizationId)) return res.status(400).send({ status: false, Message: `'${organizationId}' is not valid please check.` })
-
-    if (Object.keys(data).length == 0)
-      return res.status(400).send({
-        status: false,
-        Message: "Please provide some data for updation.",
-      });
-
-    let findRegion = await regionModel.findOne({ _id: regionId }); //isDeleted tobe added
-    if (!findRegion)
-      return res.status(400).send({
-        status: false,
-        Message: "This region is not present or might be already deleted.",
-      });
-
-    if (data.property) {
-      if (!["East", "West", "North", "South"].includes(property.region)) {
-        return res.status(400).send({
-          status: false,
-          Message: "Region must be from {North, South, East, West}",
-        });
-      }
-      // if(fieldSize != Number)
-
-      // if(fieldSize){
-      //     if( isNaN(property.fieldSize)) return res.status(400).send({ status: false, Message: "please provide fieldSize in Number." })
-      //     property.fieldSize = property.fieldSize + "sqmt"
-      // }
-    }
-
-    let newRegion = {
-      property: property,
-    };
-    let updatedFile = await regionModel.findOneAndUpdate(
-      { _id: regionId },
-      { $set: { property: property } },
-      { new: true }
-    );
-
-    return res
-      .status(200)
-      .send({ status: true, Message: "Region is updated", data: updatedFile });
-  } catch (error) {
-    return res.status(500).send({ status: false, Message: error.Message });
-  }
-};
 
 //======================get-data=================================================================================
 const getOrganization = async (req, res) => {
@@ -175,6 +93,10 @@ const getOrganization = async (req, res) => {
         let e = await cropModel.find({"soiltype": soiltype , "cropName":cropName});
       return res.status(200).send({status: true,data :e});
     }
+    if(soiltype&&season){
+        let e = await cropModel.find({"soiltype": soiltype , "season":season});
+      return res.status(200).send({status: true,data :e});
+    }
 
     if(season){
         let fieldSi = await cropModel.find({"season": season});
@@ -192,6 +114,50 @@ const getOrganization = async (req, res) => {
     return res.status(500).send({ status: false, msg: error.message });
   }
 };
+
+//=====================update-region======================================================================
+
+const updateRegion = async function (req, res) {
+  try {
+    let data = req.body;
+    let organizationId = req.params.organizationId;
+
+    let { regionId, property } = data;
+
+    if (!organizationId)
+      return res.status(400).send({status: false, Message: "Please provide the organizationId. It's mandatory.",});
+    // if (!isValidObjectId(organizationId)) return res.status(400).send({ status: false, Message: `'${organizationId}' is not valid please check.` })
+
+    if (Object.keys(data).length == 0)
+      return res.status(400).send({status: false, Message: "Please provide some data for updation.",});
+
+    let findRegion = await regionModel.findOne({ _id: regionId }); //isDeleted tobe added
+    if (!findRegion)
+      return res.status(400).send({status: false,Message: "This region is not present or might be already deleted.",});
+
+    if (data.property) {
+      if (!["East", "West", "North", "South"].includes(property.region)) {
+        return res.status(400).send({ status: false,Message: "Region must be from {North, South, East, West}", });
+      }
+      
+    }
+
+    let newRegion = {
+      property: property,
+    };
+    let updatedFile = await regionModel.findOneAndUpdate(
+      { _id: regionId },
+      { $set: { property: property } },
+      { new: true }
+    );
+
+    return res.status(200).send({ status: true, Message: "Region is updated", data: updatedFile });
+  } catch (error) {
+    return res.status(500).send({ status: false, Message: error.Message });
+  }
+};
+
+
 //================delete-Region=========================================================================================
 let deleteRegion = async function (req, res) {
   try {
@@ -199,15 +165,11 @@ let deleteRegion = async function (req, res) {
 
     let check = await regionModel.findOne({ _id: regionId, isDeletetd: false });
     if (!check)
-      return res
-        .status(400)
-        .send({ status: false, message: "No region found with this regionId" });
+      return res.status(400).send({ status: false, message: "No region found with this regionId" });
 
     let is_Deleted = check.isDeleted;
     if (is_Deleted == true)
-      return res
-        .status(404)
-        .send({ status: false, message: "Region is already Deleted " });
+      return res.status(404).send({ status: false, message: "Region is already Deleted " });
 
     await regionModel.findOneAndUpdate(
       { _id: regionId },
@@ -215,9 +177,7 @@ let deleteRegion = async function (req, res) {
       { new: true }
     );
 
-    return res
-      .status(200)
-      .send({ status: true, message: "Region Deleted Successfully" });
+    return res.status(200).send({ status: true, message: "Region Deleted Successfully" });
   } catch (error) {
     return res.status(500).send({ status: false, Message: error.Message });
   }
